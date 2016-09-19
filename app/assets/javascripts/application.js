6fefb4376e2fa5abba09b53f9a5ca5650ef3b1e3
AnnHryt <hrytsyuka@gmail.com>
2016-09-18 23:34:46 -0700

    Added devise to support user login  and links to Login and Sign Up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,4 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require twitter/bootstrap
-//= require turbolinks
-//= require_tree .
+//= require bootstrap-sprockets
